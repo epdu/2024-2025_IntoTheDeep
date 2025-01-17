b5afc93f4204ca7566aabf87fb0b04dc62af76e4
Olawumi <olawumikaka@gmail.com>
2025-01-16 19:35:39 -0500

    1 Specimen, 3 sample.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SpecimenSampleParkChassis.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/SpecimenSampleParkChassis.kt
@@ -25,7 +25,7 @@ class SpecimenSampleParkChassis : OpMode() {
 
     override fun start() {
         runBlocking(scoringArm.collect())
-        runBlocking(specimenClaw.open(1.0))
+        runBlocking(specimenClaw.open(0.5))
         runBlocking(specimenClaw.close(0.5))
         runBlocking(scoringArm.score())
         runBlocking(
@@ -38,51 +38,51 @@ class SpecimenSampleParkChassis : OpMode() {
         runBlocking(specimenClaw.open())
         runBlocking(
             drive.actionBuilder(drive.getPose())
-                .strafeTo(Vector2d(-4.0, 54.0))
+                .strafeTo(Vector2d(-8.0, 54.0))
                 .build()
         )
         runBlocking(specimenClaw.close())
         runBlocking(scoringArm.collect())
-        runBlocking(specimenClaw.approach())
+        //runBlocking(specimenClaw.approach())
+        //runBlocking(
+        //    drive.actionBuilder(drive.getPose())
+        //        .setTangent(Math.PI / 2)
+        //        .splineToLinearHeading(Pose2d(Vector2d(-27.0, 59.0), Math.PI), Math.PI)
+        //        .strafeTo(Vector2d(-35.0, 59.0))
+        //        .build()
+        //)
+        //runBlocking(specimenClaw.open())
+        //runBlocking(specimenClaw.close())
+        //runBlocking(scoringArm.score())
+        //runBlocking(
+        //    drive.actionBuilder(drive.getPose())
+        //        .setTangent(0.0)
+        //        .splineToLinearHeading(Pose2d(Vector2d(-4.0, 55.0), -Math.PI / 2), -Math.PI / 2)
+        //        .strafeTo(Vector2d(-4.0, 28.0))
+        //        .build()
+        //)
+        //runBlocking(specimenClaw.open())
         runBlocking(
             drive.actionBuilder(drive.getPose())
+                //.waitSeconds(0.3)
+                //.strafeTo(Vector2d(-6.0, 44.0))
+                //.waitSeconds(0.3)
                 .setTangent(Math.PI / 2)
-                .splineToLinearHeading(Pose2d(Vector2d(-27.0, 59.0), Math.PI), Math.PI)
-                .strafeTo(Vector2d(-35.0, 59.0))
-                .build()
-        )
-        runBlocking(specimenClaw.open())
-        runBlocking(specimenClaw.close())
-        runBlocking(scoringArm.score())
-        runBlocking(
-            drive.actionBuilder(drive.getPose())
-                .setTangent(0.0)
-                .splineToLinearHeading(Pose2d(Vector2d(-4.0, 55.0), -Math.PI / 2), -Math.PI / 2)
-                .strafeTo(Vector2d(-4.0, 28.0))
-                .build()
-        )
-        runBlocking(specimenClaw.open())
-        runBlocking(
-            drive.actionBuilder(drive.getPose())
-                .waitSeconds(1.0)
-                .strafeTo(Vector2d(-6.0, 44.0))
-                .waitSeconds(1.0)
-                .setTangent(Math.PI / 2)
+                .splineToConstantHeading(Vector2d(-24.0, 54.0), Math.PI)
                 .splineToConstantHeading(Vector2d(-24.0, 48.0), Math.PI)
                 .splineToConstantHeading(Vector2d(-49.0, 14.0), Math.PI)
                 .build()
         )
 
-        //Actions.runBlocking(drive.actionBuilder(drive.getPose())
-//
-        //        .strafeTo(new Vector2d(-46.0,60.0)) //go to 1st sample
-        //        .strafeTo(new Vector2d(-46.0,7.0)) //go to 1st sample
-        //        .strafeTo(new Vector2d(-60.0,7.0)) //strafe over
-        //        .strafeTo(new Vector2d(-55.0,60.0))//go to 2nd sample
-        //        .strafeTo(new Vector2d(-55.0,7.0))//go to 2nd sample
-        //        .strafeTo(new Vector2d(-64.0,7.0)) //strafe over
-        //        .strafeTo(new Vector2d(-64.0,60.0)) //go to 3rd sample
-        //        .build());
+        runBlocking(drive.actionBuilder(drive.getPose())
+                .strafeToConstantHeading(Vector2d(-46.0,60.0)) //go to 1st sample
+                .strafeToConstantHeading(Vector2d(-46.0,7.0)) //go to 1st sample
+                .strafeToConstantHeading(Vector2d(-60.0,7.0)) //strafe over
+                .strafeToConstantHeading(Vector2d(-55.0,60.0))//go to 2nd sample
+                .strafeToConstantHeading(Vector2d(-55.0,7.0))//go to 2nd sample
+                .strafeToConstantHeading(Vector2d(-63.0,7.0)) //strafe over
+                .strafeToConstantHeading(Vector2d(-63.0,60.0)) //go to 3rd sample
+                .build());
     }
 
     override fun loop() {
