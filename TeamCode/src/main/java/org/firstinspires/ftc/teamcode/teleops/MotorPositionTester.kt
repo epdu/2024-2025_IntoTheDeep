d55a7701a8a19b51183c6a9ee74221e4657086a7
pandaLaptop <nrrobotics@gmail.com>
2025-01-25 12:59:41 -0500

    Added Hanging to MotorPositionTester

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/MotorPositionTester.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/MotorPositionTester.kt
@@ -12,6 +12,7 @@ import com.qualcomm.robotcore.hardware.DcMotorEx
 class MotorPositionTester : OpMode() {
     private lateinit var collectionArm: DcMotorEx
     private lateinit var scoringArm: DcMotorEx
+    private lateinit var hanging: DcMotorEx
     private lateinit var g2: PandaGamepad
     //stop being slow and flipping init please
 
@@ -20,20 +21,24 @@ class MotorPositionTester : OpMode() {
         g2 = PandaGamepad(gamepad2)
         collectionArm = hardwareMap.get(DcMotorEx::class.java, "collectionArm")
         scoringArm = hardwareMap.get(DcMotorEx::class.java, "scoringArm")
+        hanging = hardwareMap.get(DcMotorEx::class.java, "hanging")
         collectionArm.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
         scoringArm.mode = DcMotor.RunMode.STOP_AND_RESET_ENCODER
         collectionArm.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
         scoringArm.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
+        hanging.mode = DcMotor.RunMode.RUN_WITHOUT_ENCODER
     }
 
     override fun loop() {
         g2.update()
 
-        collectionArm.power = gamepad2.left_stick_y.toDouble()
+        hanging.power = gamepad2.left_stick_y.toDouble()
         scoringArm.power = gamepad2.right_stick_y.toDouble()
 
         telemetry.addData("scoringArm", scoringArm.currentPosition)
         telemetry.addData("collectionArm", collectionArm.currentPosition)
+        telemetry.addData("collectionArm", hanging.currentPosition)
+
     }
 
 
